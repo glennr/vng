9fa8199d1c67d7459d403955a9a4ccf550d24913
Glenn Roberts <glenn.roberts@siyelo.com>
2012-07-03 15:42:13 +0200

    add countdown - unstyled

--- site/js/script.js
+++ site/js/script.js
@@ -1,7 +1,19 @@
 /* Author:
 
 */
-
+// Initiate Countdown
+jQuery(document).ready(function() {
+  $('#countdown_dashboard').countDown({
+    targetDate: {
+      'day': 		1,
+      'month': 	8,
+      'year': 	2012,
+      'hour': 	12,
+      'min': 		0,
+      'sec': 		0
+    }
+  });
+});
 
 
 
